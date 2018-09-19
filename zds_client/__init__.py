63fe9c4d741641dd721ab45f753f337f3e3d3a62
Sergei Maertens <sergei@maykinmedia.nl>
2018-09-19 17:14:55 +0200

    Extracting params for paths in the API spec implemented

--- zds_client/__init__.py
+++ zds_client/__init__.py
@@ -1,7 +1,8 @@
 from pkg_resources import get_distribution
 
 from .client import Client
+from .schema import extract_params, get_operation_url
 
 __version__ = get_distribution('gemma-zds-client').version
 
-__all__ = ['Client']
+__all__ = ['Client', 'extract_params', 'get_operation_url']
--- zds_client/schema.py
+++ zds_client/schema.py
@@ -5,7 +5,7 @@ DEFAULT_PATH_PARAMETERS = {
 }
 
 
-def get_operation_url(spec: dict, operation: str, **kwargs) -> str:
+def get_operation_url(spec: dict, operation: str, pattern_only=False, **kwargs) -> str:
     url = spec['servers'][0]['url']
     base_path = urlparse(url).path
 
@@ -17,7 +17,36 @@ def get_operation_url(spec: dict, operation: str, **kwargs) -> str:
             if method['operationId'] == operation:
                 format_kwargs = DEFAULT_PATH_PARAMETERS.copy()
                 format_kwargs.update(**kwargs)
-                path = path.format(**format_kwargs)
+                if not pattern_only:
+                    path = path.format(**format_kwargs)
                 return '{base_path}{path}'.format(base_path=base_path, path=path)
 
     raise ValueError('Operation {operation} not found'.format(operation=operation))
+
+
+def path_to_bits(path: str) -> list:
+    return [bit for bit in reversed(path.split('/')) if bit]
+
+
+def extract_params(url: str, pattern: str) -> dict:
+    """
+    Given an actual url and a pattern, extract the matching parameters.
+
+    Example:
+
+    >>> pattern = '/api/v1/zaken/{uuid}'
+    >>> url = 'https://example.com/zrc/api/v1/zaken/1234'
+    >>> extract_params(url, pattern)
+    {'uuid': '1234'}
+    """
+    path_url = urlparse(url).path
+    path_pattern = urlparse(pattern).path
+
+    # pattern should be shortest, since actual urls may be hosted on a subpath
+    pattern_bits = path_to_bits(path_pattern)
+    url_bits = path_to_bits(path_url)[:len(pattern_bits)]
+
+    return {
+        pattern[1:-1]: value for pattern, value in zip(pattern_bits, url_bits)
+        if pattern != value
+    }
