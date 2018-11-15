6b572ea7e3ec740a4b382cd0bfa6c18265c03bd5
Sergei Maertens <sergei@maykinmedia.nl>
2018-11-15 11:53:40 +0100

    :boom: Refactored Client.request to return parsed json
    Breaking change - Client.request no longer returns a requests.Response
    object, but a list or dict.

--- zds_client/client.py
+++ zds_client/client.py
@@ -4,6 +4,7 @@ import re
 import shutil
 import subprocess
 import tempfile
+from typing import Any, Dict, List, Union
 from urllib.parse import urljoin, urlparse
 
 import requests
@@ -14,6 +15,8 @@ from .log import Log
 from .schema import get_operation_url
 
 logger = logging.getLogger(__name__)
+
+Object = Dict[str, Any]
 
 UUID_PATTERN = re.compile(
     r'[0-9a-f]{8}\-[0-9a-f]{4}\-4[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12}',
@@ -213,8 +216,20 @@ class Client:
             self.fetch_schema()
         return self._schema
 
-    def request(self, path: str, operation: str, method='GET', **kwargs):
+    def request(self, path: str, operation: str, method='GET', expected_status=200,
+                **kwargs) -> Union[List[Object], Object]:
+        """
+        Make the HTTP request using requests.
+
+        The URL is created based on the path and base URL and any defaults
+        from the OAS schema are injected.
+
+        :return: a list or dict, the result of calling response.json()
+        :raises: :class:`requests.HTTPException` for internal server errors
+        :raises: :class:``ClientError` for HTTP 4xx status codes
+        """
         url = urljoin(self.base_url, path)
+
         headers = kwargs.pop('headers', {})
         headers.setdefault('Accept', 'application/json')
         headers.setdefault('Content-Type', 'application/json')
@@ -224,6 +239,7 @@ class Client:
             headers.update(self.auth.credentials())
 
         kwargs['headers'] = headers
+
         response = requests.request(method, url, **kwargs)
 
         try:
@@ -237,8 +253,8 @@ class Client:
             method,
             headers,
             kwargs.get('data', kwargs.get('json', None)),
-            response.status_code if response else None,
-            dict(response.headers) if response else None,
+            response.status_code,
+            dict(response.headers),
             response_json,
             params=kwargs.get('params'),
         )
@@ -250,7 +266,8 @@ class Client:
                 raise
             raise ClientError(response_json) from exc
 
-        return response
+        assert response.status_code == expected_status, response_json
+        return response_json
 
     def fetch_schema(self):
         url = urljoin(self.base_url, 'schema/openapi.yaml')
@@ -261,30 +278,22 @@ class Client:
         swagger2openapi = Swagger2OpenApi(self.base_dir, response.content)
         self._schema = swagger2openapi.convert()
 
-    def list(self, resource: str, query_params=None, **path_kwargs):
+    def list(self, resource: str, query_params=None, **path_kwargs) -> List[Object]:
         operation_id = '{resource}_list'.format(resource=resource)
         url = get_operation_url(self.schema, operation_id, **path_kwargs)
-        response = self.request(url, operation_id, params=query_params)
-        assert response.status_code == 200, response.json()
-        return response.json()
+        return self.request(url, operation_id, params=query_params)
 
-    def retrieve(self, resource: str, url=None, **path_kwargs):
+    def retrieve(self, resource: str, url=None, **path_kwargs) -> Object:
         operation_id = '{resource}_read'.format(resource=resource)
         if url is None:
             url = get_operation_url(self.schema, operation_id, **path_kwargs)
-        response = self.request(url, operation_id)
-        assert response.status_code == 200, response.json()
-        return response.json()
+        return self.request(url, operation_id)
 
-    def create(self, resource: str, data: dict, **path_kwargs):
+    def create(self, resource: str, data: dict, **path_kwargs) -> Object:
         operation_id = '{resource}_create'.format(resource=resource)
         url = get_operation_url(self.schema, operation_id, **path_kwargs)
-        response = self.request(url, operation_id, method='POST', json=data)
-        assert response.status_code == 201, response.json()
-        return response.json()
+        return self.request(url, operation_id, method='POST', json=data, expected_status=201)
 
-    def operation(self, operation_id: str, data: dict, **path_kwargs):
+    def operation(self, operation_id: str, data: dict, **path_kwargs) -> Union[List[Object], Object]:
         url = get_operation_url(self.schema, operation_id, **path_kwargs)
-        response = self.request(url, operation_id, method='POST', json=data)
-        assert response.status_code == 200, response.json()
-        return response.json()
+        return self.request(url, operation_id, method='POST', json=data)
