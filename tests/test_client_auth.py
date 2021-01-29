52b307b83a7248572b0e2aac8516f558ba7d4dcf
Sergei Maertens <sergei@maykinmedia.nl>
2021-01-29 10:26:59 +0100

    :fire: Cleaned up ClientAuth class implementation
    Deprecations are still left there, which will be removed in 1.0

--- tests/test_client_auth.py
+++ tests/test_client_auth.py
@@ -1,3 +1,6 @@
+import base64
+import json
+
 import jwt
 
 from zds_client import ClientAuth
@@ -27,3 +30,18 @@ def test_client_id_in_body():
     assert "client_id" in payload
     assert "zds" not in payload
     assert payload["client_id"] == "client id"
+
+
+def test_explicit_alg():
+    auth = ClientAuth("dummy", "dummy")
+    credentials = auth.credentials()["Authorization"]
+    token = credentials.split(" ")[1]
+    header = token.split(".")[0].encode()
+    # taken from django.utils.http.urlsafe_base64_decode
+    decoded = base64.urlsafe_b64decode(
+        header.ljust(len(header) + len(header) % 4, b"=")
+    )
+
+    jwt_header = json.loads(decoded)
+
+    assert jwt_header["alg"] == "HS256"
