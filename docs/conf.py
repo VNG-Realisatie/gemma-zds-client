7e16679389a86f88980cbd5059154c7dad458440
Sergei Maertens <sergei@maykinmedia.nl>
2021-03-16 12:15:05 +0100

    :bug: Fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+source_dir = os.path.join(os.path.dirname(os.path.abspath(".")), "zds_client")
+
+sys.path.insert(0, source_dir)
 
 
 # -- Project information -----------------------------------------------------
